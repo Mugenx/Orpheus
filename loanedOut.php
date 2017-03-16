853c957d78c91e6e395d96f9f2eb0564d927fa23
linrez <lin00096@algonquinlive.com>
2017-03-16 18:42:35 -0400

    Add files via upload

---
+++ loanedOut.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+    <head>
+
+    </head>
+    <body>
+                <h1>Loaned Out</h1>
+                <?php
+
+                $servername = "localhost";
+                $username = "username";
+                $password = "password";
+                $dbname = "orpheus";
+
+                $conn = new mysqli($servername, $username, $password, $dbname);
+
+                if($conn->connect_error)
+                {
+                    die("Connection failed: " . $conn->connect_error);
+                }
+
+                $sql = "SELECT * FROM tLoanedOut";
+                $result = $conn->query($sql);
+
+                if($result->num_rows > 0)
+                {
+                    echo "<table border = '1'>";
+                        echo "<tr>";
+                            echo "<th colspan = '4'><em> Loaned Out </em></th>";
+                        echo "</tr>";
+
+                        echo "<tr>";
+                        echo "<td>Equipment Tag</td>";
+                        echo "<td>Student Number</td>";
+                        echo "<td>Loaned Date</td>";
+                        echo "<td>Due Date</td>";
+                        echo "</tr>";
+
+                    while($row = $result->fetch_assoc())
+                    {
+                        echo "<tr>";
+                            echo "<td>" . $row['fEquipmentTag'] . "</td>";
+                            echo "<td>" . $row['fStudentNumber'] . "</td>";
+                            echo "<td>" . $row['fLoanedDate'] . "</td>";
+                            echo "<td>" . $row['fDueDate'] . "</td>";
+                        echo "</tr>";
+                    }
+
+                    echo "</table";
+                }
+
+                $conn->close();
+
+                ?>
+    </body>
+</html>
---
+++ reports.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    </head>
+    <body>
+                <div class="main">
+                    <h1>Reports</h1>
+                    
+                    <a href="overdue.php">OverDue</a>
+		    <a href="loanedOut.php">Loaned Out</a>
+		    <a href="loanHistory.php">Loan Hisotry</a>
+
+                </div>
+    </body>
+</html>
