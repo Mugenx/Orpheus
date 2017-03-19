bf48d965e85d9bb8576927e51961eeece7ee988d
linrez <lin00096@algonquinlive.com>
2017-03-19 14:12:58 -0400

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,25 @@ session_start();
 <!DOCTYPE html>
 <html>
 <head>
+<meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=Edge,chrome=1" />
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Orhpeus</title>
 
+  <!-- Bootstrap core CSS -->
+  <!-- Latest compiled and minified CSS -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <!-- jQuery library -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <!-- Latest compiled JavaScript -->
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+  <!-- jquery -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+
+  <!-- css -->
+  <script src="js/toTop.js"></script>
+  <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
 
