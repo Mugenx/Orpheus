abfd4d47d182df2656452ba1fd4ded341f4bb7ec
kennyganton <kennyganton@hotmail.com>
2017-03-19 12:16:03 -0400

    Add files via upload
    My currentusers page for prototype 2 without the errors

--- CurrentUsers.php
+++ CurrentUsers.php
@@ -48,23 +48,23 @@
         $servername = 'localhost';
         $username   = 'wp_eatery';
         $password   = 'password';
-        $dbname     = 'loansystemtest';
+        $dbname     = 'alms';
 
         $conn = new mysqli($servername, $username, $password, $dbname);
         //------------------------------------------------------------//
 
         //--------------------------GET STUDENT INFO FROM DATABASE-------------------------//
-        $studentInfoQuery  = "SELECT * FROM tUsers WHERE fStudentNumber = 040828055";
+        $studentInfoQuery  = "SELECT * FROM Users WHERE StudentNumber = 040828055";
         $studentInfoResult = $conn->query($studentInfoQuery);
         while ($row = $studentInfoResult->fetch_assoc()) {
-            $studentNumber = $row['fStudentNumber'];
-            $userName      = $row['fUserName'];
-            $firstName     = $row['fFirstName'];
-            $lastName      = $row['fLastName'];
-            $email         = $row['fEmail'];
-            $program       = $row['fProgram'];
-            $year          = $row['fYear'];
-            $notes         = $row['fNotes'];
+            $studentNumber = $row['StudentNumber'];
+            $userName      = $row['UserName'];
+            $firstName     = $row['FirstName'];
+            $lastName      = $row['LastName'];
+            $email         = $row['Email'];
+            $program       = $row['Program'];
+            $year          = $row['Year'];
+            $notes         = $row['Notes'];
         }
         //--------------------------------------------------------------------------------//
     ?>
@@ -107,17 +107,18 @@
                 <!-Data of student's loaned equipment will go here->
                 <?php
 
-                    $loansQuery  = "SELECT tEquipment.fName, tLoanedOut.fEquipmentTag, tLoanedOut.fStudentNumber, tLoanedOut.fDueDate 
-                                    FROM   tEquipment, tLoanedOut
-                                    WHERE  tEquipment.fEquipmentTag = tLoanedOut.fEquipmentTag 
-                                    AND    tLoanedOut.fStudentNumber = '$studentNumber'";
+                    $loansQuery  = "SELECT Equipment.Name, Equipment.EquipmentTag, LoanedOut.StudentNumber, LoanedOut.DueDate 
+                                    FROM Equipment, LoanedOut 
+                                    WHERE Equipment.EquipmentRecordID = LoanedOut.EquipmentRecordID AND LoanedOut.StudentNumber = '$studentNumber'";
                     $loansResult = $conn->query($loansQuery);
-                    while($row = mysqli_fetch_array($loansResult)){
-                        echo "<tr>
-                              <td>".$row["fEquipmentTag"]."</td>
-                              <td>".$row["fName"]        ."</td>
-                              <td>".$row["fDueDate"]     ."</td>
-                              </tr>";
+                    if($loansResult != false){
+                        while($row = mysqli_fetch_array($loansResult)){
+                            echo "<tr>
+                                  <td>".$row["EquipmentTag"]."</td>
+                                  <td>".$row["Name"]        ."</td>
+                                  <td>".$row["DueDate"]     ."</td>
+                                  </tr>";
+                        }
                     }
 
                 ?>
@@ -140,28 +141,30 @@
                 </thead>
                 <?php
 
-                    $reserveQuery  = "SELECT fCategoryName, fReserveStart, fReserveEnd, fApproved, fDenied, fDeniedReason 
-                                      FROM   tReservations, tCategoryTree 
-                                      WHERE  tCategoryTree.fCategoryID = tReservations.fCategoryID 
-                                      AND    tReservations.fStudentNumber = '$studentNumber'";
+                    $reserveQuery  = "SELECT Equipment.Name, ReserveStart, ReserveEnd, Approved, Denied, DeniedReason 
+                                      FROM Reservations, Equipment 
+                                      WHERE Reservations.EquipmentRecordID = Equipment.EquipmentRecordID 
+                                      AND Reservations.StudentNumber = '$studentNumber'";
                     $reserveResult = $conn->query($reserveQuery);
-                    while($row = mysqli_fetch_array($reserveResult)){
-                        echo "<tr>
-                              <td>".$row["fCategoryName"]."</td>
-                              <td>".$row["fReserveStart"]."</td>
-                              <td>".$row["fReserveEnd"]  ."</td>
-                              <td>";
-                        if($row["fApproved"] == 1){
-                            //If reservation is approved
-                            echo  "Approved </td><td> N/A </td></tr>";
-                        }
-                        else if($row["fDenied"] == 1){
-                            //If reservation is denied
-                            echo "Denied </td><td>" . $row["fDeniedReason"]. "</td></tr>";
-                        }
-                        else{
-                            //If neither of the above are true, reservation is pending
-                            echo "Pending </td><td> N/A </td></tr>";
+                    if($reserveResult != false){
+                        while($row = mysqli_fetch_array($reserveResult)){
+                            echo "<tr>
+                                  <td>".$row["Name"]."</td>
+                                  <td>".$row["ReserveStart"]."</td>
+                                  <td>".$row["ReserveEnd"]  ."</td>
+                                  <td>";
+                            if($row["Approved"] == 1){
+                                //If reservation is approved
+                                echo  "Approved </td><td> N/A </td></tr>";
+                            }
+                            else if($row["fDenied"] == 1){
+                                //If reservation is denied
+                                echo "Denied </td><td>" . $row["DeniedReason"]. "</td></tr>";
+                            }
+                            else{
+                                //If neither of the above are true, reservation is pending
+                                echo "Pending </td><td> N/A </td></tr>";
+                            }
                         }
                     }
 
